1d0e0946be69478c91faa153265fb3e231e48a81
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-22 12:02:38 +0530

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,20 +1,30 @@
-m1 = float(input("Enter subject 1 marks: "))
-m2 = float(input("Enter subject 2 marks: "))
-m3 = float(input("Enter subject 3 marks: "))
-m4 = float(input("Enter subject 4 marks: "))
-m5 = float(input("Enter subject 5 marks: "))
+import sys
+
+# Expecting 5 marks as command-line arguments
+if len(sys.argv) != 6:
+    print("Usage: python marks.py <m1> <m2> <m3> <m4> <m5>")
+    sys.exit(1)
+
+# Read marks from arguments
+m1 = float(sys.argv[1])
+m2 = float(sys.argv[2])
+m3 = float(sys.argv[3])
+m4 = float(sys.argv[4])
+m5 = float(sys.argv[5])
 
 avg = (m1 + m2 + m3 + m4 + m5) / 5
+print("Average Marks:", avg)
 
-print("Average =", avg)
-
+# Grade logic
 if avg >= 90:
-    print("Grade = A")
+    grade = "A"
 elif avg >= 75:
-    print("Grade = B")
+    grade = "B"
 elif avg >= 60:
-    print("Grade = C")
+    grade = "C"
 elif avg >= 45:
-    print("Grade = D")
+    grade = "D"
 else:
-    print("Grade = Fail")
+    grade = "Fail"
+
+print("Grade:", grade)
